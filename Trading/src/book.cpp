9c45e5a70d8a29ca632e529b20dd4c2e918d19dd
Fontaine Arnaud <arnaud@famillefontaine.org>
2013-12-13 22:47:07 +0100

    - https://app.asana.com/0/8248998262792/8674055055798
    Classe book
    
    Affiche les quotes reçu du serveur en deux même tableau mais trié de façon décroissante pour les bid et l'autre de façon croissante pour les offer.

---
+++ Trading/src/book.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Book {
+    private:
+    string provenance[10];
+    int volume[10];
+    float bid[10];
+    float offer[10];
+
+    public:
+    Book(){
+        for(int i=0;i<10;i++){
+            provenance[i]="nothing";
+            volume[i] = 0;
+            bid[i]=0;
+            offer[i]=0;
+        }
+    }
+
+    void afficher_book(){
+        printf("Provenance/volume/Bid/Offer\n");
+        for(int i=0;i<10;i++){
+            cout << provenance[i] <<"  "<< volume[i] <<"  "<< bid[i] <<"  "<< offer[i] <<"\n";
+        }
+    }
+
+    //Ajout d'un bid, cad que l'on insert en fonction du bid pour que le tableau soit trié de façon décroissante
+    void ajouter_par_bid(string s, int v,float b,float o){
+        int i,j;
+        for(i=0;i<10;i++){
+            if(b>=bid[i]){
+                for(j=9;j>i;j--){
+                    bid[j]=bid[j-1];
+                    offer[j]= offer[j-1];
+                    provenance[j]= provenance[j-1];
+                    volume[j]= volume[j-1];
+                }
+            bid[i] = b;
+            offer[i]=o;
+            provenance[i]= s;
+            volume[i]= v;
+            break;
+            }
+        }
+    }
+
+    //Ajout d'un bid, cad que l'on insert en fonction de l'offre pour que le tableau soit trié de façon croisante
+    void ajouter_par_offer(string s, int v,float b,float o){
+        int i,j;
+        for(i=0;i<10;i++){
+            if(o>=offer[i]){
+                for(j=9;j>i;j--){
+                    bid[j]=bid[j-1];
+                    offer[j]= offer[j-1];
+                    provenance[j]= provenance[j-1];
+                    volume[j]= volume[j-1];
+                }
+            bid[i] = b;
+            offer[i]=o;
+            provenance[i]= s;
+            volume[i]= v;
+            break;
+            }
+        }
+    }
+};
+
+int main()
+{
+    Book mesBid, mesOffer;
+    mesBid.ajouter_par_bid("ibm",4,1.0452,1.2654);
+    mesBid.ajouter_par_bid("facebook",11,1.0689,1.9054);
+    mesBid.afficher_book();
+
+    mesOffer.ajouter_par_offer("ibm",4,1.0452,1.2654);
+    mesOffer.ajouter_par_offer("facebook",11,1.0689,1.1054);
+    mesOffer.afficher_book();
+    return 0;
+}
